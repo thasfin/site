4058a690090b2aa7bc43368f98224360b5e7a045
Daniela Caus <38079926+danicaus@users.noreply.github.com>
2023-02-07 22:57:10 -0300

    feat: cria componente Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../style/index.css"
 import "../style/button.css"
 import "../style/footer.css"
-import "../style/header.scss"
 import "../style/welcome.css"
 
 export default function MyApp({ Component, pageProps }) {
